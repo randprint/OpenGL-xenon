bfab8d08d5f429de0c930c7f5fc3a9301f3596bb
Ced2911 <Ced2911@gmail.com>
2012-09-28 21:39:43 +0200

    oops

---
+++ source/GL/gl_colors.c
@@ -0,0 +1,64 @@
+#include "gl_xenos.h"
+
+#define BYTE_CLAMP(i) (int) ((((i) > 255) ? 255 : (((i) < 0) ? 0 : (i))))
+
+#define COLOR_ARGB(a,r,g,b) ((unsigned int)((((a)&0xff)<<24)|(((r)&0xff)<<16)|(((g)&0xff)<<8)|((b)&0xff)))
+
+unsigned int Gl_Color_2_Xe (GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha)
+{
+	return COLOR_ARGB
+	(
+		BYTE_CLAMP (alpha * 255.0f),
+		BYTE_CLAMP (red * 255.0f),
+		BYTE_CLAMP (green * 255.0f),
+		BYTE_CLAMP (blue * 255.0f)
+	);
+}
+
+void GL_SetColor (int red, int green, int blue, int alpha)
+{
+	// overwrite color incase verts set it
+	xe_CurrentColor.u32 = Gl_Color_2_Xe(red, green, blue, alpha);
+}
+
+void glColor3f (GLfloat red, GLfloat green, GLfloat blue)
+{
+	GL_SetColor (red * 255, green * 255, blue * 255, 255);
+}
+
+
+void glColor3fv (const GLfloat *v)
+{
+	GL_SetColor (v[0] * 255, v[1] * 255, v[2] * 255, 255);
+}
+
+
+void glColor3ubv (const GLubyte *v)
+{
+	GL_SetColor (v[0], v[1], v[2], 255);
+}
+
+
+void glColor4f (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
+{
+	GL_SetColor (red * 255, green * 255, blue * 255, alpha * 255);
+}
+
+
+void glColor4fv (const GLfloat *v)
+{
+	GL_SetColor (v[0] * 255, v[1] * 255, v[2] * 255, v[3] * 255);
+}
+
+
+void glColor4ubv (const GLubyte *v)
+{
+	GL_SetColor (v[0], v[1], v[2], v[3]);
+}
+
+
+void glColor4ub (GLubyte red, GLubyte green, GLubyte blue, GLubyte alpha)
+{
+	GL_SetColor (red, green, blue, alpha);
+}
+
